af2a614336bf17bd58a394687df15c25caf6b72a
John Cacerez <johncacerez97@gmail.com>
2025-08-30 08:25:43 -0500

    Se adiciona tabla de Detalle pedidos y ajustes a la DB Finished

--- DATABASE/database.sql
+++ DATABASE/database.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS Tienda;
-USE DATABASE Tienda;
+USE Tienda;
 
 CREATE TABLE IF NOT EXISTS t_usuarios(
     id_usuario INT(11) auto_increment NOT NULL,
@@ -26,6 +26,7 @@ INSERT INTO t_categorias VALUE(NULL, 'Construcccion'),(null,'Electricos'),(null,
 CREATE TABLE IF NOT EXISTS t_productos(
     id_producto         int(11) auto_increment  NOT NULL,
     nombre_producto     varchar(100)            NOT NULL,
+    descripcion         VARCHAR(200)            NOT NULL,
     precio              float(100,2)            NOT NULL,
     stock               int(100)                NOT NULL,
     oferta              varchar(50)             NOT NULL,
@@ -39,10 +40,25 @@ CREATE TABLE IF NOT EXISTS t_productos(
 CREATE TABLE IF NOT EXISTS t_pedidos(
     id_pedido           int(11) auto_increment  NOT NULL,
     id_usuario          int(11)                 NOT NULL,
+    ciudad              varchar(35)             NOT NULL,
+    direccion           varchar(55)             NOT NULL,
+    costo               int(11)                 NOT NULL,
     fecha               date                    NOT NULL,
     hora                time                    NOT NULL,
     estado              varchar(50)             NOT NULL,
     CONSTRAINT          pk_pedido               PRIMARY KEY(id_pedido),
     CONSTRAINT          fk_pedido_usuario       FOREIGN KEY(id_usuario) REFERENCES t_usuarios(id_usuario)
 )ENGINE=InnoDb;
+
+CREATE TABLE IF NOT EXISTS detalle_pedidos(
+    id_detalle_pedidos  INT(11) auto_increment NOT NULL,
+    id_pedido           INT(11)                NOT NULL,
+    id_producto         INT(11)                NOT NULL,
+    unidades            INT(10)                NOT NULL,
+    CONSTRAINT          pk_detalle_pedidos     PRIMARY KEY(id_detalle_pedidos),
+    CONSTRAINT          fk_id_pedido           FOREIGN KEY(id_pedido) REFERENCES t_pedidos(id_pedido),
+    CONSTRAINT          fk_id_producto         FOREIGN KEY(id_producto) REFERENCES t_productos(id_producto)
+)ENGINE=InnoDb;
+
+
 
